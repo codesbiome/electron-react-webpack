b1a6c8991d5441a4886fd6932f6c6b511ce35602
Gary <gary@codesbiome.com>
2019-10-08 18:00:33 +0530

    added webpack version

--- src/main/preload.js
+++ src/main/preload.js
@@ -2,6 +2,10 @@
 // It has the same sandbox as a Chrome extension.
 console.log("Hello from 'preload'");
 
+const { version: ReactVersion } = require('react');
+const { version: WebpackVersion } = require('webpack');
+const modulesInUse = ['chrome', 'node', 'electron', 'react', 'webpack'];
+
 // Show version numbers for different technologies used
 window.addEventListener('DOMContentLoaded', () => {
   const replaceText = (selector, text) => {
@@ -9,7 +13,20 @@ window.addEventListener('DOMContentLoaded', () => {
     if (element) element.innerText = text;
   };
 
-  for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type]);
+  for (const type of modulesInUse) {
+    let version = '';
+
+    switch (type) {
+      case 'react':
+        version = ReactVersion;
+        break;
+      case 'webpack':
+        version = WebpackVersion;
+        break;
+      default:
+        version = process.versions[type];
+    }
+
+    replaceText(`${type}-version`, version);
   }
 });
