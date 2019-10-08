116cbe37a812f089c0cae104104042dfcb747fc0
Gary <gary@codesbiome.com>
2019-10-08 17:41:13 +0530

    extracted module

--- src/main/preload.js
+++ src/main/preload.js
@@ -2,17 +2,14 @@
 // It has the same sandbox as a Chrome extension.
 console.log("Hello from 'preload'");
 
-// Load system information
-module.exports = function() {
-  // Show version numbers for different technologies used
-  window.addEventListener('DOMContentLoaded', () => {
-    const replaceText = (selector, text) => {
-      const element = document.getElementById(selector);
-      if (element) element.innerText = text;
-    };
+// Show version numbers for different technologies used
+window.addEventListener('DOMContentLoaded', () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector);
+    if (element) element.innerText = text;
+  };
 
-    for (const type of ['chrome', 'node', 'electron']) {
-      replaceText(`${type}-version`, process.versions[type]);
-    }
-  });
-};
+  for (const type of ['chrome', 'node', 'electron']) {
+    replaceText(`${type}-version`, process.versions[type]);
+  }
+});
